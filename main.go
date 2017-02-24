91caf4f664cfc9a2494f41b2fb19c4b7900246b0
Ivan Chavero <ichavero@redhat.com>
2017-02-23 21:43:57 -0700

    Add command line arguments
    Added node file and debug command line arguments.

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"flag"
+	"log"
+	"net/http"
 )
 
+type config struct {
+	nodes string
+	debug bool
+}
+
+var Config config
+
 func main() {
+	nodesFlag := flag.String("nodes", "/etc/moximo/nodes", "Node config file")
+	debugFlag := flag.Bool("d", false, "Enable debugging")
+	flag.Parse()
 
-    router := NewRouter()
+	Config := &config{nodes: *nodesFlag, debug: *debugFlag}
 
-    log.Fatal(http.ListenAndServe(":8081", router))
+	log.Printf("Starting moximo master with flags:\nNodes file: %s\nDebug mode:%b\n",
+		Config.nodes, Config.debug)
+	router := NewRouter()
+
+	log.Fatal(http.ListenAndServe(":8081", router))
 }
